fa7f330c56d5be63ab159957f061882866061e99
Leandro Reis <leandro.e.reis@gmail.com>
2022-07-12 14:04:41 -0300

    chore: configure tailwindcss on store app

---
+++ apps/store/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// reference: https://nx.dev/guides/using-tailwind-css-in-react
