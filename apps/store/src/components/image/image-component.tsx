15c0f66ab38a454bf3a9813c2af1b3e86c91707e
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-07-13 10:17:34 -0300

    feat: add error image if there is no src

--- apps/store/src/components/image/image-component.tsx
+++ apps/store/src/components/image/image-component.tsx
@@ -1,20 +1,35 @@
+import { classNames } from '@tp-ecommerce-mono/utils';
 import React from 'react';
 import Column from '../column/column';
 
 export interface ImageComponentProps {
   className?: string;
-  src: string;
+  src?: string;
   alt?: string;
+  noSrcAlt?: string;
 }
 
 const ImageComponent: React.FC<ImageComponentProps> = ({
   className,
+  src,
   alt = '',
+  noSrcAlt = '',
   ...props
 }) => {
   return (
-    <Column className={className}>
-      <img className="h-fit w-fit" alt={alt} {...props} />
+    <Column className={classNames('items-center justify-center', className)}>
+      {src ? (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          className="h-fit w-fit max-h-full max-w-full"
+          src={src}
+          alt={alt}
+          {...props}
+        />
+      ) : (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img src={noSrcAlt} alt="Erro" />
+      )}
     </Column>
   );
 };
