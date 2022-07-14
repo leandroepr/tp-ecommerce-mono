3a728dbd3c6ecb739df6d50f7a7f2839b0d3e502
Dailson Lima <dailsonlima.dev@gmail.com>
2022-07-14 14:23:23 -0300

    refact: align address field

--- apps/store/src/components/default-header-content/default-header-content.tsx
+++ apps/store/src/components/default-header-content/default-header-content.tsx
@@ -6,7 +6,7 @@ const DefaultHeaderContent = () => {
       <div className="flex flex-col px-3 grow-[0]">
         <div className="text-sm py-1 mt-5">Logo placeholder</div>
 
-        <div className="flex container flex-row gap-1 py-1 w-[150px]">
+        <div className="flex container flex-row gap-1 py-2 w-[150px]">
           <div className="text-xs py-2">i</div>
           <div className="text-xs">
             Envie para Fulano, <strong>Endereço Tal, número tal</strong>
