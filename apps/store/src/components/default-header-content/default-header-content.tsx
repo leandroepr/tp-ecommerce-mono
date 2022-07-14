6e68f25b0b3cae03694d31824c185bc3b9cd47fe
Dailson Lima <dailsonlima.dev@gmail.com>
2022-07-14 14:21:20 -0300

    refact: align logo placeholder

--- apps/store/src/components/default-header-content/default-header-content.tsx
+++ apps/store/src/components/default-header-content/default-header-content.tsx
@@ -4,7 +4,7 @@ const DefaultHeaderContent = () => {
   return (
     <div className="bg-yellow-300 flex flex-row text-stone-600">
       <div className="flex flex-col px-3 grow-[0]">
-        <div className="text-sm py-4">Logo placeholder</div>
+        <div className="text-sm py-1 mt-5">Logo placeholder</div>
 
         <div className="flex container flex-row gap-1 py-1 w-[150px]">
           <div className="text-xs py-2">i</div>
